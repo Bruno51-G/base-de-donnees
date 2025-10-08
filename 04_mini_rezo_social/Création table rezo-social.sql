5ac4252e1d4e6e741f560df4cb13dcc1f1a26b9c
Bruno51-G <185206289+Bruno51-G@users.noreply.github.com>
2025-10-08 16:56:22 +0200

    exo rezo-social SQL

--- 04_mini_rezo_social/Création table rezo-social.sql
+++ 04_mini_rezo_social/Création table rezo-social.sql
@@ -1,24 +1,31 @@
 CREATE DATABASE rezo_social;
 
-DROP TABLE IF EXISTS utilisateur;
+DROP DATABASE rezo_social;
+
+rezo_socialDROP TABLE IF EXISTS utilisateur;
 DROP TABLE IF EXISTS publication;
 DROP TABLE IF EXISTS aimer;
 
+TRUNCATE TABLE utilisateur;
+TRUNCATE TABLE publication;
+TRUNCATE TABLE aimer;
+
+
 CREATE TABLE utilisateur(
 id INT,
-nom_utilisateur VARCHAR(32) UNIQUE,
-email VARCHAR(128) UNIQUE,
-PRIMARY KEY(id)
+nom_utilisateur VARCHAR(32) NOT NULL,
+email VARCHAR(128) NOT NULL,
+PRIMARY KEY(id),
+UNIQUE(nom_utilisateur, email)
 );
 
 CREATE TABLE publication(
-pub_id INT,
-pud_date DATETIME,
-pub_titre VARCHAR(255),
-pub_contenu TEXT,
+pub_id INT AUTO_INCREMENT,
+pub_date DATETIME NOT NULL,
+pub_titre VARCHAR(255) NOT NULL,
+pub_contenu TEXT NOT NULL,
 id INT,
-PRIMARY KEY(pub_id),
-UNIQUE(id)
+PRIMARY KEY(pub_id)
 );
 
 CREATE TABLE aimer(
@@ -38,8 +45,15 @@ FOREIGN KEY(pub_id) REFERENCES publication(pub_id);
 
 
 
-INSERT INTO utilisateur VALUE
+INSERT INTO utilisateur (id,nom_utilisateur,email) VALUES 
 ('1', 'Zorro', 'zorb@example.com'),
 ('2', 'Patchouli', 'patchouli@example.fr'),
 ('3', 'Eva', 'eva.stt@example.com');
+
+INSERT INTO publication (pub_date,pub_titre,pub_contenu,id) VALUES 
+('2024-11-23 14:30', 'Il fait beau', 'Quel beau soleil aujourd’hui !', '3'),
+('2024-11-23 09:15', 'Les bonbons', 'Les bonbons, c’est bon !', '2'),
+('2024-11-27 08:17', 'Super resto', 'J’ai découvert un super restaurant hier soir.', '1'),
+('2024-12-05 17:52', 'Album disponible', 'Nouvel album de mon groupe préféré !', '2'),
+('2024-12-15 10:05', 'Aidez-moi', 'Je cherche une recette de gâteau au chocolat.', '3');
 
