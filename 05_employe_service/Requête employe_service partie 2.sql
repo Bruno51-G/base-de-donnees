791c8b5e9a74b1745ef2e85890afb8544c59199f
Bruno51-G <185206289+Bruno51-G@users.noreply.github.com>
2025-10-21 12:52:17 +0200

    suite des requêtes, exo employe service

--- 05_employe_service/Requête employe_service partie 2.sql
+++ 05_employe_service/Requête employe_service partie 2.sql
@@ -48,28 +48,48 @@ SELECT ENAME, HIREDATE, (SELECT HIREDATE FROM emp WHERE ENAME='FORD') AS "Date e
 FROM emp WHERE HIREDATE = (SELECT HIREDATE FROM emp WHERE ENAME='FORD');
 
 /* 10. Lister les employés ayant le même manager que CLARK. */
-SELECT ENAME AS Nom, EMPNO AS "Numéro employé", (SELECT ENAME AS "Nom", JOB, MGR FROM emp WHERE ENAME = 'CLARK') AS "Manager de CLARK"
+SELECT ENAME AS Nom, EMPNO AS "Numéro employé", MGR AS "Numéro manager", (SELECT ENAME FROM emp WHERE EMPNO = 7839) AS "Manager"
 FROM emp WHERE MGR = (SELECT MGR FROM emp WHERE ENAME = 'CLARK');
 
 /* 11. Lister les employés ayant même job et même manager que TURNER. */
+SELECT ENAME AS Nom, EMPNO AS "Numéro employé", JOB, (SELECT ENAME FROM emp WHERE EMPNO = 7698) AS Manager 
+FROM emp WHERE JOB = (SELECT job FROM emp WHERE ENAME = 'TURNER') AND MGR = (SELECT MGR FROM emp WHERE ENAME = 'TURNER') AND ENAME != 'TURNER';
 
 
 /* 12. Lister les employés du département RESEARCH embauchés le même jour que quelqu'un du département SALES. */
+SELECT emp.ENAME, emp.DEPTNO, dept.DNAME, emp.HIREDATE
+FROM emp INNER JOIN dept ON emp.DEPTNO = dept.DEPTNO
+WHERE dept.DNAME = 'RESEARCH' AND emp.HIREDATE IN
+(SELECT emp.HIREDATE FROM emp INNER JOIN dept ON emp.DEPTNO = dept.DEPTNO WHERE dept.DNAME = 'SALES');
 
 
 /* 13. Lister le nom des employés et également le nom du jour de la semaine correspondant à leur date d'embauche. */
-
+SET lc_time_names = 'fr_FR';
+SELECT ENAME AS "Nom des employés", DATE_FORMAT (HIREDATE, '%W') AS "Jour d'embauche"
+FROM emp
 
 /* 14. Donner, pour chaque employé, le nombre de mois qui s'est écoulé entre leur date d'embauche et la date actuelle. */
+SELECT ENAME AS "Nom des employés", HIREDATE AS "Date d'embauche", TIMESTAMPDIFF(MONTH, HIREDATE, CURRENT_DATE) AS "Mois écoulé entre l'embauche et date actuelle"
+FROM emp;
 
 
 /* 15. Afficher la liste des employés ayant un M et un A dans leur nom. */
+SELECT ENAME AS "Nom employé avec un M et un A dans leur nom"
+FROM emp
+WHERE ENAME LIKE '%M%' AND ENAME LIKE '%A%';
 
 
 /* 16. Afficher la liste des employés ayant deux 'A' dans leur nom. */
+SELECT ENAME AS "Nom employé avec 2 A dans leur nom"
+FROM emp
+WHERE ENAME RLIKE '[Aa].*[Aa]';
 
 
 /* 17. Afficher les employés embauchés avant tous les employés du département 10. */
+SELECT ENAME, HIREDATE AS "Date d'embauche des autres département", 
+(SELECT MIN(HIREDATE) FROM emp WHERE DEPTNO = 10) AS "Date d'embauche la plus ancienne du département 10"
+FROM emp
+WHERE HIREDATE < ALL (SELECT HIREDATE FROM emp WHERE DEPTNO = 10);
 
 
 /* 18. Sélectionner le métier où le salaire moyen est le plus faible. */
